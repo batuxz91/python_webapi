6d9114d3d7b8598d682fd0aef42b17ea8bcd5bed
CMaydana <cesar.maydana@artekium.com>
2024-03-06 01:13:08 -0300

    readme

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # FastApi
+Documentacion oficial: https://fastapi.tiangolo.com/
+
+# Instalación
+FastAPI: pip install fastapi
+Uvicorn: pip install "uvicorn[standard]"
+
+# Uvicorn
+uvicorn main:app --reload
+
+# Documentacion
+http://127.0.0.1:8000/docs
--- main.py
+++ main.py
@@ -1,8 +1,20 @@
+# Documentacion oficial: https://fastapi.tiangolo.com/
+
+# Instalacion FastAPI: pip install fastapi
+# Instalacion Uvicorn: pip install "uvicorn[standard]"
+
 from fastapi import FastAPI
 
 app = FastAPI()
 
 
 @app.get("/")
-async def read_root():
+async def root():
     return {"Hello World"}
+
+#
+
+
+@app.get("/url")
+async def url():
+    return {"url:https://google.com"}
